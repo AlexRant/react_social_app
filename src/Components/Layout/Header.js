179faeaf91505223ef13057980d6837a50e6f5a5
Alex Sirenko <aleksirenko@gmail.com>
2019-11-06 12:40:00 +0300

    modified App, added header and footer component. Add them in Layout and Components folders

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// function Header() {
+//   return (
+//     <div className="App-header">
+      
+//         <a href="#" className="nav_home">Home</a>
+//         <a href="#" className="nav_tel">Tel:</a>
+//         <a href="#" className="nav_contacts">Contacts</a>
+        
+//     </div>
+//   );
+// }
+
+// export default Header;
+
+export default props => 
+  <div>
+    Header
+  </div>
