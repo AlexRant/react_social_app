94f2c520dcdff932607a4edd9988d03ce29f0e81
Alex Sirenko <aleksirenko@gmail.com>
2019-11-13 16:27:34 +0300

    Modified Content component

--- src/Components/Excercises/Content.js
+++ src/Components/Excercises/Content.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Grid, Paper, Typography } from '@material-ui/core';
-// import LeftPane from './LeftPane';
-// import RightPane from './RightPane';
+import { List, ListItem, ListItemText } from '@material-ui/core';
 
 const styles = {
-  Paper: {  padding: 20, marginTop: 10, marginBottom: 10 }
+  Paper: {  padding: 20,
+            marginTop: 10,
+            marginBottom: 10,
+            height: 500,
+            overflowY: 'auto' 
+          }
 }
 
 export default ({ exercises }) => 
@@ -12,15 +16,37 @@ export default ({ exercises }) =>
     <Grid item sm>
       <Paper style={styles.Paper}>
       {exercises.map(([group, exercises]) =>
-        <Typography>
+      <Fragment>
+        <Typography 
+          variant="h5"
+          style={{textTransform: 'capitalize'}}
+          >
           {group}
         </Typography>
+        <List component="ul">
+          {exercises.map(({ title }) =>
+            <ListItem button>
+              <ListItemText primary={title} />
+            </ListItem>
+          )}
+        </List>
+      </Fragment>
       )}        
       </Paper>
     </Grid>
     <Grid item sm>
       <Paper style={styles.Paper}>
-        Right Panel
+        <Typography 
+          variant="h4"
+        >
+          Welcome!
+        </Typography>
+        <Typography 
+          variant="subtitle1"
+          style={{marginTop: 20 }}
+          >
+          Please select an exercise from the list on the left.
+        </Typography>
       </Paper>
     </Grid>
   </Grid>
